866cdfb311fa5d833c36fbce0c066abcf663a681
Nathan Voong <nathan.voong@clearscore.com>
2023-01-03 18:14:53 +0000

    Deleted unecessary comments in romanNumerals file

--- src/lib/romanNumerals.js
+++ src/lib/romanNumerals.js
@@ -1,24 +1,3 @@
-// function convertToRomanNumerals(num) {
-//
-//     const numerals = [[1000, 'M'], [900, 'CM'],[500, 'D'],
-//         [400, 'CD'], [100, 'C'], [90, 'XC'],
-//         [50, 'L'], [40, 'XL'], [10, 'X'],
-//         [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']];
-//     let result = "";
-//
-//     for (let i in numerals) {
-//         if (num/numerals[i][0] >= 1) {
-//             let e = Math.floor(num/numerals[i][0]);
-//             num -= e*numerals[i][0];
-//             for (let j = 0; j < e; j++) {
-//                 result += numerals[i][1];
-//             }
-//         }
-//     }
-//     return result;
-//
-// }
-
 function convertToRomanNumerals(num) {
 
     const numerals = [[1000, 'M'], [900, 'CM'],[500, 'D'],
